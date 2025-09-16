ca58638b958d485131244eb59ccc0c399be07dc7
Tarig Mekhazni <tmk-dev@hotmail.com>
2025-09-16 13:48:31 +0200

    reorganisation

--- api/config.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  server: {
-    port: parseInt(process.env.PORT || "3333"),
-    // allowedOrigins: process.env.ALLOWED_ORIGINS || "*",
-    // jwtSecret: process.env.JWT_SECRET || "jwt-secret",
-    // secure: process.env.NODE_ENV === "production" || false,
-    // logLevel: process.env.LOG_LEVEL || "info",
-  },
-};
--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "./config.js";
-import { app } from "./src/app.js";
-
-// Démarre un serveur
-const port = config.server.port;
-
-app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`)
-});
